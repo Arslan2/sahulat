1e74cda174da1711e1b8d287f351063e775fabb4
Arslan Rao <Arslanrao94@gmail.com>
2019-07-29 13:47:53 +0500

    Updated text and theme

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Myapp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
-  root to: 'questions#list'
+  #root to: 'questions#list'
+  root to: 'dashboards#dashboard_1'
 
   # All routes
   get "dashboards/dashboard_1"
