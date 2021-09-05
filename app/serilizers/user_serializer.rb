b809dd1e1bcf20e9aeba8672fa4a8ee72527571f
Arslan Rao <Arslanrao94@gmail.com>
2021-09-06 02:48:38 +0500

    Added tehsil id

--- app/serilizers/user_serializer.rb
+++ app/serilizers/user_serializer.rb
@@ -1,3 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email, :phone, :created_at, :active, :role
+  attributes :id, :first_name, :last_name, :email, :phone, :created_at, :active, :role, :tehsil_id
+
+  def tehsil_id
+  	self.object.tehsils.last.try(:id)
+  end
 end
