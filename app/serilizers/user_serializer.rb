5189a10c1a4e0426417b593e8a3ed6a988c49a48
Arslan Rao <Arslanrao94@gmail.com>
2021-09-06 12:01:54 +0500

    Added ddho krk account

--- app/serilizers/user_serializer.rb
+++ app/serilizers/user_serializer.rb
@@ -2,6 +2,11 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :email, :phone, :created_at, :active, :role, :tehsil_id
 
   def tehsil_id
-  	self.object.tehsils.last.try(:id)
+  	id = self.object.tehsils.last.try(:id)
+  	if self.object.phone == 'ddho.krk'
+  		id = Tehsil.where(name: "KRK").last.try(:id)
+  	end
+
+  	id
   end
 end
