647a8564e40e457902060bfb5d77234e94df8528
Arslan Rao <Arslanrao94@gmail.com>
2021-09-06 15:08:19 +0500

    Added iv kasur account

--- app/serilizers/user_serializer.rb
+++ app/serilizers/user_serializer.rb
@@ -4,7 +4,9 @@ class UserSerializer < ActiveModel::Serializer
   def tehsil_id
   	id = self.object.tehsils.last.try(:id)
 
-  	if self.object.phone == 'ddho.krk'
+  	if self.object.phone == 'iv.kasur'
+  		id = Tehsil.where(name: "Kasur").last.try(:id)
+  	elsif self.object.phone == 'ddho.krk'
   		id = Tehsil.where(name: "KRK").last.try(:id)
   	end
 
